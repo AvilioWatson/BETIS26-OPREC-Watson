cbbdd2cfa62dd3d34c4b6f174804fc5d3e7759f3
Hakim Nizami <hakimnizami15@gmail.com>
2026-01-10 22:17:31 +0700

    style: added feedback colors

--- app/modules/LandingPageModule/index.tsx
+++ app/modules/LandingPageModule/index.tsx
@@ -3,9 +3,16 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
-import { Home, Star } from "lucide-react";
+import {
+  AlertCircleIcon,
+  CheckCircle2Icon,
+  Home,
+  PopcornIcon,
+  Star,
+} from "lucide-react";
 import { Button } from "~/components/ui/button";
 import { Chip } from "~/components/ui/chip";
+import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 
 export function LandingPageModule() {
   return (
@@ -74,6 +81,36 @@ export function LandingPageModule() {
               <p>Add to library</p>
             </TooltipContent>
           </Tooltip>
+        </div>
+      </div>
+      <div>
+        Alert
+        <div className="flex flex-col">
+          <Alert>
+            <CheckCircle2Icon />
+            <AlertTitle>Success! Your changes have been saved</AlertTitle>
+            <AlertDescription>
+              This is an alert with icon, title and description.
+            </AlertDescription>
+          </Alert>
+          <Alert>
+            <PopcornIcon />
+            <AlertTitle>
+              This Alert has a title and an icon. No description.
+            </AlertTitle>
+          </Alert>
+          <Alert variant="destructive">
+            <AlertCircleIcon />
+            <AlertTitle>Unable to process your payment.</AlertTitle>
+            <AlertDescription>
+              <p>Please verify your billing information and try again.</p>
+              <ul className="list-inside list-disc text-sm">
+                <li>Check your card details</li>
+                <li>Ensure sufficient funds</li>
+                <li>Verify billing address</li>
+              </ul>
+            </AlertDescription>
+          </Alert>
         </div>
       </div>
     </main>
